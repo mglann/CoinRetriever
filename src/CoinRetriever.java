edb017d7a6ea1c053a555785b73af6a6bedf3b5f
mglann <mglann22@sjnd.org>
2022-03-31 09:11:35 -0700

    Broken getSymbols() method
    idk doesnt work lmao

--- src/CoinRetriever.java
+++ src/CoinRetriever.java
@@ -1,21 +1,27 @@
+import java.io.IOException;
+
 public class CoinRetriever
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
         UserInterface ui = new UserInterface();
         CoinRetrieverRequest req = new CoinRetrieverRequest();
+        //initialize symbol list from API
+        req.getSymbols();
 
         while(true)
         {
             String mainResponse = ui.inputMode();
-            if (mainResponse.equals("1"))
-            {
-                System.out.println("Ticker");
-            }
-            else
+            if (mainResponse.equals("Q"))
             {
                 System.out.println("Quit");
                 break;
+            }
+            else
+            {
+                req.setSymbol(mainResponse);
+                String apiResponse = req.getData();
+                System.out.println("API response:" + apiResponse);
             }
         }
     }
--- src/CoinRetrieverRequest.java
+++ src/CoinRetrieverRequest.java
@@ -1,25 +1,54 @@
-//import org.json.simple.JSONObject;
-//import org.json.simple.parser.*;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.*;
 import java.net.*;
-import java.util.Map;
+import java.util.*;
 import java.io.*;
 
 public class CoinRetrieverRequest
 {
+    private String symbolUrl =  "https://www.cryptonator.com/api/currencies";
+    private static final String URL = "https://api.cryptonator.com/api/ticker";
     private String symbol;
+    ArrayList<String> symbols =  new ArrayList<String>();
 
-    public void setTicker()
+    public void getSymbols() throws Exception
+    {
+        CoinRetrieverRequest symbol = new CoinRetrieverRequest();
+        URL symbolUrl = new URL(symbol.symbolUrl);
+        BufferedReader in  = new BufferedReader(new InputStreamReader(symbolUrl.openStream()));
+        java.net.URL jsonData =  symbolUrl;
+        JSONParser parser =  new JSONParser();
+        JSONArray CoinRetrieverRequest = (JSONArray)(parser.parse(in));
+
+        for(int i = 0; i < symbols.size(); i++)
+        {
+            Map m = (Map)CoinRetrieverRequest.get(i);
+            symbols.add("code");
+        }
+    }
+
+    public void setSymbol(String mainResponse)
     {
         this.symbol = symbol;
     }
 
-    public String getTicker()
-    {
-        return symbol;
-    }
 
     public boolean validateSymbol()
     {
-        return true;
+        String urlSymbols = "";
+        String getSymbols = symbols.get();
+        for(int i = 0; i < symbols.size(); i++)
+        {
+            if(urlSymbols == getSymbols)
+            {
+                return true;
+            }
+        }
+
+    }
+
+    public String getData()
+    {
+        return "data";
     }
 }
